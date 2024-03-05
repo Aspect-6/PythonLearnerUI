05c49c3688978f9a375ad18b720a2d4cd42af865
Aspect-6 <neelpatel246624@gmail.com>
2024-03-05 11:15:42 -0500

    simplify code and add a another arg to function to generalize it more

--- funcs/generate_dict.py
+++ funcs/generate_dict.py
@@ -3,7 +3,7 @@ import re
 from typing import Union
 
 
-def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None):
+def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None, type: str = None):
 
     # Create default color values
     color_counts = {
@@ -13,13 +13,10 @@ def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None):
         "green": 1,
         "blue": 1,
         "purple": 1,
-        "white": 1,
     }
     non_color_count = 1
 
     split_string = re.split(r'\s', colors_dict[name]["str"])
-
-    # print(split_string, "\n")
 
     if pre_run is not None:
         split_string: list[str] = pre_run(split_string)
@@ -29,15 +26,14 @@ def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None):
     concat_string = ""
 
     for word in split_string:
-        # print(word)
         found_color: bool = False
         for key in colors_dict[name]["dict"]:
             for item in colors_dict[name]["dict"][key]:
-                if item in word.lower():
-                    if not concat_string == "":
-                        return_dict[non_color_count] = concat_string
-                        non_color_count += 1
-                        concat_string = ""
+                if not concat_string == "":
+                    return_dict[non_color_count] = concat_string
+                    non_color_count += 1
+                    concat_string = ""
+                if item in word or item in word.lower():
                     return_dict[f"{key}_{color_counts[key]}"] = f"{word} "
                     color_counts[key] += 1
                     found_color = True
@@ -48,6 +44,6 @@ def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None):
     return_dict[non_color_count] = concat_string
 
     if post_run is not None:
-        return_dict = post_run(return_dict)
+        return_dict = post_run(return_dict, type)
 
     return return_dict
