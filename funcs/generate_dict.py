44b7aad979c4e179ef641ff4b7c64c054d32d61b
Aspect-6 <neelpatel246624@gmail.com>
2024-03-16 12:42:36 -0400

    update generate_dict function to be more readable

--- funcs/generate_dict.py
+++ funcs/generate_dict.py
@@ -3,7 +3,6 @@ import re
 
 
 def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None, index_key: str = None):
-    # Create default color values
     color_counts = {
         "red": 1,
         "orange": 1,
@@ -14,28 +13,30 @@ def generate_dict(colors_dict: dict, name: str, pre_run=None, post_run=None, ind
     }
     non_color_count = 1
 
-    split_string = re.split(r'\s', colors_dict[name]["str"])
+    split_string = colors_dict[name]["str"].split()
 
     if pre_run is not None:
         split_string: list[str] = pre_run(split_string)
 
     return_dict = OrderedDict()
 
+    flat_dict = {item: key for key in colors_dict[name]["dict"] for item in colors_dict[name]["dict"][key]}
+    
     concat_string = ""
 
     for word in split_string:
         found_color: bool = False
-        for key in colors_dict[name]["dict"]:
-            for item in colors_dict[name]["dict"][key]:
-                if not concat_string == "":
-                    return_dict[non_color_count] = concat_string
-                    non_color_count += 1
-                    concat_string = ""
-                if item in word or item in word.lower():
-                    return_dict[f"{key}_{color_counts[key]}"] = f"{word} "
-                    color_counts[key] += 1
-                    found_color = True
-                    break  # Exit the loop once a color is found
+        word_lower = word.lower()
+        for item, color in flat_dict.items():
+            if not concat_string == "":
+                return_dict[non_color_count] = concat_string
+                non_color_count += 1
+                concat_string = ""
+            if item in word or item in word_lower:
+                return_dict[f"{color}_{color_counts[color]}"] = f"{word} "
+                color_counts[color] += 1
+                found_color = True
+                break
         if not found_color:
             concat_string += word + " "
 
